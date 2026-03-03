6777a610cda2f520e91df0aceca9b1936d09af3a
swapnil <swapnilsbandagi@gmail.com>
2026-03-04 00:03:55 +0530

    this time i used subprocess to run pytest, that is amazing bro...

--- app.py
+++ app.py
@@ -3,7 +3,12 @@
 
 import subprocess
 
-print("Main App Running...")
-print("Opening Calculator...\n")
+result = subprocess.run(
+    ["python", "-m", "pytest", "-v"],
+    capture_output=True,
+    text=True
+)
 
-subprocess.run(["python", "calculator.py"])
+print("Return Code:", result.returncode)
+print("Output:\n", result.stdout)
+print("Errors:\n", result.stderr)
