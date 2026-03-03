e51ff270b94689fdfebff7b977fa79530d3c448b
swapnil <swapnilsbandagi@gmail.com>
2026-03-03 23:50:14 +0530

    i tried subprocess and pytest successful ohhhh..

--- calculator.py
+++ calculator.py
@@ -1,12 +1,13 @@
+# calculator.py
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
 
-if __name__ == "__main__":
-    x = int(input("Enter first number: "))
-    y = int(input("Enter second number: "))
-    
-    print("Addition:", add(x, y))
-    print("Subtraction:", subtract(x, y))
+def multiply(a, b):
+    return a + b
+
+def divide(a, b):
+    return a / b
---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 4 , "here add test failed "
+
+def test_subtract():
+    assert subtract(5, 3) == 2 , "here subtract test failed "
+
+def test_multiply():
+    assert multiply(4, 3) == 12  , "here multiply test failed "
+
+def test_divide():
+    assert divide(10, 2) == 5  , "here divide test failed "
